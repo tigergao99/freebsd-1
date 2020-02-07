2387d62ab2127d10037c4eababae6982a99d7598
tigergao99 <tigergao1999@outlook.com>
2020-02-07 12:34:14 -0500

    code review

--- contrib/elftoolchain/addr2line/addr2line.c
+++ contrib/elftoolchain/addr2line/addr2line.c
@@ -420,7 +420,7 @@ check_range(Dwarf_Debug dbg, Dwarf_Die die, Dwarf_Unsigned addr,
 {
 	Dwarf_Error de;
 	Dwarf_Unsigned addr_base, lopc, hipc;
-	Dwarf_Off off, ranges_off;
+	Dwarf_Off ranges_off;
 	Dwarf_Signed ranges_cnt;
 	Dwarf_Ranges *ranges;
 	int i, ret;
@@ -451,17 +451,11 @@ check_range(Dwarf_Debug dbg, Dwarf_Die die, Dwarf_Unsigned addr,
 				hipc = ~0ULL;
 			}
 
-			if (dwarf_dieoffset(die, &off, &de) != DW_DLV_OK) {
-				warnx("dwarf_dieoffset failed: %s",
-					dwarf_errmsg(de));
-				return (DW_DLV_ERROR);
-			}
-
 			if (addr >= lopc && addr < hipc) {
 				in_range = true;
 			}
 		}
-	} else if (ret == (DW_DLV_OK)) {
+	} else if (ret == DW_DLV_OK) {
 		ret = dwarf_get_ranges(dbg, ranges_off, &ranges,
 			&ranges_cnt, NULL, &de);
 		if (ret != DW_DLV_OK)
@@ -486,6 +480,7 @@ check_range(Dwarf_Debug dbg, Dwarf_Die die, Dwarf_Unsigned addr,
 
 			if (addr >= lopc && addr < hipc){
 				in_range = true;
+				break;
 			}
 		}
 	} else {
